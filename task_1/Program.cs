9bbaf82a346bfa7804954c6ea9e8963fd5611f60
c8ler <kotovsergey@yandex.ru>
2022-05-01 14:21:41 +0300

    4 dz

---
+++ task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.Write("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = int.Parse(Console.ReadLine());
+if(num1 > num2 & num1 > num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+if(num2 > num1 & num2 > num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
+if(num3 > num1 & num3 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num3);
+}
